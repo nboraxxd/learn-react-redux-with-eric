45e234a8919019be83373cedf4b18163f933cbb5
bora <nhatbaoxxd@gmail.com>
2023-04-30 13:21:09 +0700

    react router
    - navigate to admin page
    - navigate to user page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './styles/reset.css'
 import './App.scss'
 import Header from './Header/Header'
@@ -6,7 +7,18 @@ import Header from './Header/Header'
 const App = () => {
   return (
     <div>
-      <Header></Header>
+      <Header />
+      <div>
+        test link{' '}
+        <div>
+          <button>
+            <Link to="/users">Go to user page</Link>
+          </button>
+          <button>
+            <Link to="/admins">Go to admin page</Link>
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
