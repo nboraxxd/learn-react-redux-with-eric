478df4a29a8d68cf1e904e00e272122791df1d87
bora <nhatbaoxxd@gmail.com>
2023-05-02 09:52:37 +0700

    connect api add new user

--- src/components/Admin/Content/ManageUser.jsx
+++ src/components/Admin/Content/ManageUser.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FiUserPlus } from 'react-icons/fi'
 import ModalAddNewUser from './ModalAddNewUser'
+import './ManageUser.scss'
 
 const ManageUser = () => {
+  const [showModalAddNewUser, setShowModalAddNewUser] = useState(false)
+
+  function handleOnClickAddNewUserBtn() {}
+
   return (
-    <div className="manage-user-container">
-      <div className="manage-user-title"></div>
+    <div className="manage-user">
+      <h1 className="manage-user-title">Manage user</h1>
       <div className="manage-user-content">
-        <button className="manage-user-button">Add new user</button>
-        <ModalAddNewUser />
+        <button
+          className="btn btn-primary manage-user-btn"
+          onClick={() => setShowModalAddNewUser(true)}
+        >
+          <FiUserPlus style={{ fontSize: '22px' }} /> Add new user
+        </button>
+        <ModalAddNewUser show={showModalAddNewUser} setShow={setShowModalAddNewUser} />
         <div className="manage-user-table">table user</div>
       </div>
     </div>
